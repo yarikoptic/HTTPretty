834e20b1e25b157b90e0bd10630e0ef9126f16da
Karel Jakubec <karel@apiwatcher.com>
2015-12-10 21:23:37 +0100

    Add latest_requests() to public interface

--- httpretty/__init__.py
+++ httpretty/__init__.py
@@ -55,6 +55,10 @@ def last_request():
     """returns the last request"""
     return httpretty.last_request
 
+def latest_requests():
+    """Returns list of latest requests made"""
+    return httpretty.latest_requests
+
 def has_request():
     """returns a boolean indicating whether any request has been made"""
     return not isinstance(httpretty.last_request.headers, EmptyRequestHeaders)
